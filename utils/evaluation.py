d3d7cd5a1209d9546e9675bd1220850b02854d5c
claudiarichardxx <claudiarichardxx@gmail.com>
2024-01-28 01:55:44 -0500

    changes

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,33 +1,35 @@
 from collections import Counter
 
-def getResults(df):
+class Evaluation:
 
-  k_values = []
-  for row in df['k_list']:
-      k_values.extend(row)
-  results = Counter(k_values)
-  for i in results.keys():
-    print('k = ', i,'in', results[i], 'search(es)')
+  def getResults(self, df):
 
-  return results
+    k_values = []
+    for row in df['k_list']:
+        k_values.extend(row)
+    results = Counter(k_values)
+    for i in results.keys():
+      print('k =', i,'in', results[i], 'search(es)')
+
+    return results
 
 
-def getAverageSuccessValues(results, total, k_vals):
+  def getAverageSuccessValues(self, results, total, k_vals):
 
-  success_at_k = {}
-  for i in range(0, len(k_vals)):
-    success_at_k[k_vals[i]] = 0
-    for j in results.keys():
-          till = k_vals[i]
-          start = 0
-          if(j > start and j<= till):
-            success_at_k[k_vals[i]] += results[j]
+    success_at_k = {}
+    for i in range(0, len(k_vals)):
+      success_at_k[k_vals[i]] = 0
+      for j in results.keys():
+            till = k_vals[i]
+            start = 0
+            if(j > start and j<= till):
+              success_at_k[k_vals[i]] += results[j]
 
-  for i in success_at_k.keys():
-    success_at_k[i] = round( success_at_k[i]/total * 100, 2)
-    print('Success at', i, 'is', success_at_k[i],'%')
+    for i in success_at_k.keys():
+      success_at_k[i] = round( success_at_k[i]/total * 100, 2)
+      print('Success at', i, 'is', success_at_k[i],'%')
 
-  return success_at_k
+    return success_at_k
 
 
 
--- utils/setup.py
+++ utils/setup.py
@@ -3,63 +3,55 @@ import urllib
 import nltk
 from nltk.corpus import wordnet as wn
 nltk.download('wordnet')
-import sys
+from Collections import Counter
 
-def createData(link, filename = "data\\data.dat"):
+class DataHandling():
 
-  urllib.request.urlretrieve(link, filename)
-  current_key = None
-  result_dict = {}
-  repeats = []
-  print('About the spellcheck dataset:\n')
-  data_file = open(filename, 'r')
-  print(filename)
-  for line in data_file.readlines():
+  def countWords(self, lst):
+      return len(lst)
 
-      lines = line.strip().split('\n')
+  def describeData(self, df, repeats, word_count):
+      
+      df['num_of_words'] = df['misspellings'].apply(self.countWords)
+      print('\nThere are a total of', len(df), 'unique correct words and', len(repeats) , 'repeats which totals to', len(df) + len(repeats),'words')
+      print('There are a total of', df.num_of_words.sum(), 'misspelt words in the dataset')
+      print('\nAbout the wordnet dictionary:\n')
+      print('There are', word_count,'words in the wordnet dictionary\n')
 
-      if lines[0].startswith('$'):
-              current_key = lines[0][1:].lower()
-              if(current_key not in result_dict.keys()):
-                  result_dict[current_key] = []
-              else:
-                print('The word "' + current_key + '" occurs more than once')
-                repeats.append(current_key)
-                #result_dict[current_key].append(lines[0].lower())
-              #print(line[1:-2])
-              #result_dict[current_key] = []
-      else:
-              result_dict[current_key].append(lines[0].lower())
+  def buildDictionary(self):
+      
+      dictionary = {}
+      count = 0
+      for word in wn.words(lang='eng'):
 
-  df = pd.DataFrame([result_dict.keys(), result_dict.values()]).T
-  df.columns = ['words', 'misspellings']
+        if (word[0].lower() not in dictionary.keys()):
+            dictionary[word[0].lower()] = set()
 
-  return df, repeats
+        dictionary[word[0].lower()].add(word.lower())
+        count = count +1
+        
+      for i in dictionary.keys():
+            dictionary[i] = sorted(dictionary[i], key=len)
 
-def countWords(lst):
-  return len(lst)
-
-def describeData(df, repeats, word_count):
-  
-  df['num_of_words'] = df['misspellings'].apply(countWords)
-  print('\nThere are a total of', len(df), 'unique correct words and', len(repeats) , 'repeats which totals to', len(df) + len(repeats),'words')
-  print('There are a total of', df.num_of_words.sum(), 'misspelt words in the dataset')
-  print('\nAbout the wordnet dictionary:\n')
-  print('There are', word_count,'words in the wordnet dictionary\n')
-
-def buildDictionary():
-  
-  dictionary = {}
-  count = 0
-  for word in wn.words(lang='eng'):
-
-    if (word[0].lower() not in dictionary.keys()):
-        dictionary[word[0].lower()] = set()
-
-    dictionary[word[0].lower()].add(word.lower())
-    count = count +1
+      return dictionary, count
     
-  for i in dictionary.keys():
-        dictionary[i] = sorted(dictionary[i], key=len)
+    
 
-  return dictionary, count
+  def findStarters(self, words, misspellings, pos):
+      for i in misspellings:
+          target = words[0]
+          present = i[0]
+          if (present not in pos.keys()):
+              pos[present] = []
+              #count = count + 1
+          pos[present].append(target)
+      return len(misspellings)
+    
+  def findPossibleFirstLetters(self, df):
+      pos = {}
+      df['num_of_words'] = df.apply(lambda x: self.findStarters(x.words, x.misspellings, pos), axis = 1)
+      for i in pos.keys():
+        ll = Counter(pos[i])
+        pos[i] = sorted(ll, key = ll.get, reverse = True)
+      return df, pos
+
