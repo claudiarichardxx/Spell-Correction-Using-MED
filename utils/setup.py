bad70042e7ba63f7ac0668da88b3c2aee0443139
claudiarichardxx <claudiarichardxx@gmail.com>
2024-01-28 12:40:35 -0500

    modified:   utils/tools.py

--- utils/setup.py
+++ utils/setup.py
@@ -5,49 +5,52 @@ from nltk.corpus import wordnet as wn
 nltk.download('wordnet')
 from collections import Counter
 
-def countWords(lst):
-      return len(lst)
+class SetupFunctions:
 
-def describeData(df, repeats, word_count):
+      def countWords(self, lst):
+            return len(lst)
+
+      def describeData(self, df, repeats, word_count):
+            
+            df['num_of_words'] = df['misspellings'].apply(self.countWords)
+            print('\nThere are a total of', len(df), 'unique correct words and', len(repeats) , 'repeats which totals to', len(df) + len(repeats),'words')
+            print('There are a total of', df.num_of_words.sum(), 'misspelt words in the dataset')
+            print('\nAbout the wordnet dictionary:\n')
+            print('There are', word_count,'words in the wordnet dictionary\n')
+
+      def buildDictionary(self):
+            
+            dictionary = {}
+            count = 0
+            for word in wn.words(lang='eng'):
+
+                  if (word[0].lower() not in dictionary.keys()):
+                        dictionary[word[0].lower()] = set()
+
+                  dictionary[word[0].lower()].add(word.lower())
+                  count = count +1
+                  
+            for i in dictionary.keys():
+                  dictionary[i] = sorted(dictionary[i], key=len)
+
+            return dictionary, count
       
-      df['num_of_words'] = df['misspellings'].apply(countWords)
-      print('\nThere are a total of', len(df), 'unique correct words and', len(repeats) , 'repeats which totals to', len(df) + len(repeats),'words')
-      print('There are a total of', df.num_of_words.sum(), 'misspelt words in the dataset')
-      print('\nAbout the wordnet dictionary:\n')
-      print('There are', word_count,'words in the wordnet dictionary\n')
+      def findStarters(words, misspellings, pos):
 
-def buildDictionary():
+            for i in misspellings:
+                  target = words[0]
+                  present = i[0]
+                  if (present not in pos.keys()):
+                        pos[present] = []
+                        #count = count + 1
+                  pos[present].append(target)
+            return len(misspellings)
       
-      dictionary = {}
-      count = 0
-      for word in wn.words(lang='eng'):
-
-        if (word[0].lower() not in dictionary.keys()):
-            dictionary[word[0].lower()] = set()
-
-        dictionary[word[0].lower()].add(word.lower())
-        count = count +1
-        
-      for i in dictionary.keys():
-            dictionary[i] = sorted(dictionary[i], key=len)
-
-      return dictionary, count
-    
-def findStarters(words, misspellings, pos):
-      for i in misspellings:
-          target = words[0]
-          present = i[0]
-          if (present not in pos.keys()):
-              pos[present] = []
-              #count = count + 1
-          pos[present].append(target)
-      return len(misspellings)
-    
-def findPossibleFirstLetters(df):
-      pos = {}
-      df['num_of_words'] = df.apply(lambda x: findStarters(x.words, x.misspellings, pos), axis = 1)
-      for i in pos.keys():
-        ll = Counter(pos[i])
-        pos[i] = sorted(ll, key = ll.get, reverse = True)
-      return df, pos
+      def findPossibleFirstLetters(self, df):
+            pos = {}
+            df['num_of_words'] = df.apply(lambda x: self.findStarters(x.words, x.misspellings, pos), axis = 1)
+            for i in pos.keys():
+                  ll = Counter(pos[i])
+                  pos[i] = sorted(ll, key = ll.get, reverse = True)
+            return df, pos
 
