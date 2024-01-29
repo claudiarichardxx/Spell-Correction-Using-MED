66dddce1af982bab4391fa80f6ece6d0b9707ed7
CaptainSubhram <149985240+CaptainSubhram@users.noreply.github.com>
2024-01-29 17:38:05 -0500

    Update main.py comments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#This module is the main part of the code and is the entry point to the application.
+
+#Importing the libraries and supporting modules
 import nltk
 import pandas as pd
 from run import SpellCheck
@@ -6,7 +9,7 @@ nltk.download('wordnet')
 
 
 if __name__ == "__main__":
-
+    #Call to the supporting functions
     sp  = SpellCheck()
     pandarallel.initialize(nb_workers = 300)
     links = { 'birkbeck' : 'https://www.dcs.bbk.ac.uk/~roger/missp.dat',
